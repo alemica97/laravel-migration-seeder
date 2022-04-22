1cc18d3326fd4aecff2244f8e66eae4429030b66
alemica97 <alessandro.micalizzi08@gmail.com>
2022-04-22 18:34:45 +0200

    tramite i seeders e faker ho popolato la tabella con elementi generati da faker

--- database/migrations/2022_04_22_144110_create_trains_table.php
+++ database/migrations/2022_04_22_144110_create_trains_table.php
@@ -16,8 +16,8 @@ class CreateTrainsTable extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('company',50);
-            $table->string('departure_station',100);
-            $table->string('arrival_station',100);
+            $table->string('departure_station',50);
+            $table->string('arrival_station',50);
             $table->time('departure_time');
             $table->time('arrival_time');
             $table->string('train_code',30);
