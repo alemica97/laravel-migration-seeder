0d2b088ab7fc58f71854cdb6577154533ad3693c
alemica97 <alessandro.micalizzi08@gmail.com>
2022-04-22 18:45:07 +0200

    tramite un ciclo for fatto nel seeder ho creato direttamente 30 treni

--- database/seeds/TrainsTableSeeders.php
+++ database/seeds/TrainsTableSeeders.php
@@ -13,18 +13,21 @@ class TrainsTableSeeders extends Seeder
      */
     public function run(Faker $faker)
     {
-        $train = new Train();
 
-        $train->company = $faker->word();
-        $train->departure_station = $faker->state();
-        $train->arrival_station = $faker->state();
-        $train->departure_time = $faker->time();
-        $train->arrival_time = $faker->time();
-        $train->train_code = $faker->isbn10();
-        $train->carriages_number = $faker->numberBetween(3,30);
-        $train->is_ontime = $faker->numberBetween(0,1);
-        $train->deleted = $faker->numberBetween(0,1);
-
-        $train->save();
+        for($i = 0; $i < 30; $i++){
+            $train = new Train();
+            
+            $train->company = $faker->word();
+            $train->departure_station = $faker->state();
+            $train->arrival_station = $faker->state();
+            $train->departure_time = $faker->time();
+            $train->arrival_time = $faker->time();
+            $train->train_code = $faker->isbn10();
+            $train->carriages_number = $faker->numberBetween(3,30);
+            $train->is_ontime = $faker->numberBetween(0,1);
+            $train->deleted = $faker->numberBetween(0,1);
+        
+            $train->save();
+        }
     }
 }
