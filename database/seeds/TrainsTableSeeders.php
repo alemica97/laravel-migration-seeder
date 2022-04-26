9d2b280fc77ba1655523fd4256e27750d18b6aeb
alemica97 <alessandro.micalizzi08@gmail.com>
2022-04-26 10:32:13 +0200

    correzione di alcuni $faker per ottenere date più vicine ad oggi

--- database/seeds/TrainsTableSeeders.php
+++ database/seeds/TrainsTableSeeders.php
@@ -17,13 +17,13 @@ class TrainsTableSeeders extends Seeder
         for($i = 0; $i < 200; $i++){
             $train = new Train();
 
-            $train->company = $faker->word();
-            $train->departure_station = $faker->state();
-            $train->arrival_station = $faker->state();
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
             $train->departure_time = $faker->time();
             $train->arrival_time = $faker->time();
-            $train->departure_date = $faker->date();
-            $train->train_code = $faker->isbn10();
+            $train->departure_date = $faker->dateTimeBetween('-1 week', '+1 week');
+            $train->train_code = $faker->bothify('??-######');
             $train->carriages_number = $faker->numberBetween(3,30);
             $train->is_ontime = $faker->numberBetween(0,1);
             $train->deleted = $faker->numberBetween(0,1);
